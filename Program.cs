4b3c690870f5660500d64f2fae0ede8496fbdf24
wsr14 <wsr14@K218PCREZERV2>
2022-04-08 14:24:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public delegate string HelloDelegate(string name);
+    class SomeClass
+    {
+        public string SayHello(string s)
+        {
+            return $"Привет,{s}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeClass someClass = new SomeClass();
+            HelloDelegate hD = new HelloDelegate(someClass.SayHello);
+            Console.WriteLine(hD.Invoke(""));
+            Console.ReadKey();
+        }
+    }
+}
